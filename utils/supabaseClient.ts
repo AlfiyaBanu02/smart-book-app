a6606384021419ac231b45d9008abfb47341d01e
AlfiyaBanu02 <allualfi02@gmail.com>
2026-02-18 14:48:19 +0530

    Fix dashboard and supabase connection

--- utils/supabaseClient.ts
+++ utils/supabaseClient.ts
@@ -1,7 +1,5 @@
-// utils/supabaseClient.ts
 import { createClient } from '@supabase/supabase-js'
 
-// Using environment variables for safety
 export const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
 export const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 export const supabase = createClient(supabaseUrl, supabaseKey)
